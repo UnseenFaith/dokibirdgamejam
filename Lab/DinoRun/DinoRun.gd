ff22544eac90bcf0a8aa27a53bdd63d36e3b74af
UnseenFaith <1832323+UnseenFaith@users.noreply.github.com>
2025-08-14 20:38:58 -0400

    level1 outro scene

--- Lab/DinoRun/DinoRun.gd
+++ Lab/DinoRun/DinoRun.gd
@@ -24,6 +24,8 @@ var CURRENT_SPEED := 200
 
 var cutscenePlayed = Settings.get('firstCutscenePlayed')
 
+var gameEnded = false
+
 func _ready() -> void:
 	$"Player-Platformer/InputComponent".isEnabled = false
 	$AnimationPlayer.play("cutscene")
@@ -37,8 +39,11 @@ func _start_Dialogic() -> void:
 		Dialogic.start("timeline")
 
 func timeline_ended() -> void:
-	$AnimationPlayer.stop()
-	_start_game()
+	if not gameEnded:
+		$AnimationPlayer.stop()
+		_start_game()
+	else:
+		print("Transition Back to Desktop Here")
 	
 func _start_game() -> void:
 	$Floor/Dirt.autoscroll.x = - CURRENT_SPEED
@@ -71,8 +76,6 @@ func _process(delta: float) -> void:
 			$Tracker/Path2D/PathFollow2D.progress_ratio = (current_time / total_time)
 		else:
 			$Tracker/Path2D/PathFollow2D.progress_ratio = 0.0
-	else:
-		$Tracker/Path2D/PathFollow2D.progress_ratio = 0.0
 
 func generate_obstacle(obst = null) -> void:
 	var index
@@ -93,3 +96,29 @@ func onObstacleTimer_timeout() -> void:
 	generate_obstacle()
 	if player.position.x < $Midpoint.position.x && player.bodyComponent.isOnFloor:
 		player.velocity.x += 100
+
+
+func onTracker_finished() -> void:
+	print("Game Won!")
+	
+	$"Player-Platformer/InputComponent".isEnabled = false
+	var tween = create_tween()
+	$Crow.visible = true
+	$Crow.position = Vector2(459, 120)
+	tween.tween_property($Enemy, "position", Vector2(-50, 220), 2.0)
+	tween.parallel().tween_property($"Player-Platformer", "position", $Midpoint.position, 1.0)
+	tween.parallel().tween_property($Crow, "position", Vector2(195, 120), 2.0)
+	
+	await get_tree().create_timer(3.0).timeout
+	
+	$Floor/Dirt.autoscroll.x = 0
+	$Floor/Tumble.visible = false
+	$Background.process_mode = Node.PROCESS_MODE_DISABLED
+	$Tracker.visible = false
+	$"Player-Platformer/DokiAnimationComponent".isEnabled = false
+	$"Player-Platformer/AnimatedSprite2D".play("idle")
+	
+	$AntiVirus.visible = true
+	$AntiVirus/Player.play("virus")
+	
+	Dialogic.start("level1_outro")
--- Lab/DinoRun/Scenes/Tracker.gd
+++ Lab/DinoRun/Scenes/Tracker.gd
@@ -1,5 +1,8 @@
 extends Node2D
 
+signal finished()
+
 func _process(_delta: float) -> void:
-	if $Path2D/PathFollow2D.progress_ratio == 1:
+	if $Path2D/PathFollow2D.progress_ratio >= 0.99992477893829:
+		finished.emit()
 		self.process_mode = Node.PROCESS_MODE_DISABLED
